96a1103ce9d6b6cd674bc75161292bed8d2e5821
Mark Rae <rae.mark@gmail.com>
2012-09-07 22:35:49 -0700

    Revised README and made it a .txt file

---
+++ WoDdice.py
@@ -0,0 +1,17 @@
+import random
+##outcome can be a dramatic failure, failure, n successes, exceptional success
+def roll(dicepool):
+    outcome = range(dicepool)
+    for n in outcome:
+        outcome[n] = random.randint(1,10)
+    successes = 0
+    for n in outcome:
+        if
+    
+    if dicepool == 0: ##chance die. success only on 10
+
+##def instantAction(dicepool)
+##def extendedAction(dicepool)
+##def roteAction(dicepool)
+        
+    
