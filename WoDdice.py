a7d8da2e5a7f6e4e78d41aa4c299ee3c5e9b74a2
Mark Rae <rae.mark@gmail.com>
2012-09-13 08:18:07 -0700

    Corrected chance rolls
    In chance rolls, a 1 is only a dramatic failure if it occurs on the
    first roll, 10's are rerolled as normal but only 10's are ever
    considered successes.

--- WoDdice.py
+++ WoDdice.py
@@ -1,14 +1,15 @@
 import random
-##return value can be a dramatic failure, failure, n successes, exceptional success
+##results can be a dramatic failure, failure, n successes, exceptional success
 ##dramatic failure will return -1. failure will return 0. n successes will return a positive integer.
 def roll(dicepool):
-    if dicepool >= 0:
+    ## Regular dice roll
+    if dicepool > 0:
         ##main roll
         outcome = range(dicepool)
         for n in outcome:
             outcome[n] = random.randint(1,10)
         print outcome
-        
+
         ##reroll 10's
         numrerolled = 0
         for n in outcome:
@@ -23,27 +24,41 @@ def roll(dicepool):
 
         ##count successes
         successes = 0
-        if dicepool == 0:
-            ##chance die. success only on 10. 10-again?
-            outcome = [random.randint(1,10)]
+        ##normal 10-again roll. 8, 9 and 10 are sucesses
+        for n in outcome:
+            if n == 10:
+                successes += 1
+            if n == 9:
+                successes += 1
+            if n == 8:
+                successes += 1
+                
+    ## Chance die
+    elif dicepool <= 0:
+        ##chance die. success only on 10. 10-again
+        outcome = [random.randint(1,10)]
+        print outcome
+        if outcome == [1]:
+            ##dramatic failure only if you roll 1 on your initial chance roll
+            successes = -1
+        if outcome == [10]:
+            ##reroll 10's
+            numrerolled = 1
+            while numrerolled > 0:
+                newdie = random.randint(1,10)
+                if newdie != 10:
+                    numrerolled -= 1
+                outcome += [newdie]
+                print outcome
+            ##count successes
+            successes = 0
+            ##only 10's are sucesses, even on rerolls
             for n in outcome:
                 if n == 10:
-                    sucesses +=1
-                if n == 1:
-                    sucesses = -1
-        elif dicepool >= 1:
-            ##normal 10-again roll. 8, 9 and 10 are sucesses
-            for n in outcome:
-                if n == 10:
                     successes += 1
-                if n == 9:
-                    successes += 1
-                if n == 8:
-                    successes += 1
-        return successes
+        ##else: regular failure
+    return successes
 
 ##def instantAction(dicepool)
 ##def extendedAction(dicepool)
 ##def roteAction(dicepool)
-        
-    
